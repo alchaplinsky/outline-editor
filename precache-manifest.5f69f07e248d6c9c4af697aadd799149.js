bbcf522476feb54bfad0ba9a4b63c1ed34bd70ea
Alex Chaplinsky <alchaplinsky@gmail.com>
2019-04-27 23:51:05 +0200

    Updates

---
+++ precache-manifest.5f69f07e248d6c9c4af697aadd799149.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed3f3c44b89dab56ae8ec8a59015d9f",
+    "url": "/outliner/index.html"
+  },
+  {
+    "revision": "20c9daeedd34eb60a233",
+    "url": "/outliner/static/css/main.07f9c57f.chunk.css"
+  },
+  {
+    "revision": "4ff58e407b8b1263a66e",
+    "url": "/outliner/static/js/2.c05fe66e.chunk.js"
+  },
+  {
+    "revision": "20c9daeedd34eb60a233",
+    "url": "/outliner/static/js/main.bd3b0441.chunk.js"
+  },
+  {
+    "revision": "895b9d557df8732878f5",
+    "url": "/outliner/static/js/runtime~main.273144e2.js"
+  }
+]);
